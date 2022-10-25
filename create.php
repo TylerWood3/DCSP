51ee5398c16db7c6182332e2bcacd3c41de05c52
TylerWood3 <63980345+TylerWood3@users.noreply.github.com>
2022-10-24 22:39:10 -0500

    Add files via upload

---
+++ create.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Your Account Was Succesfully Created!</title>
+    <style>
+        .error 
+		{
+          color: #FF0000;
+        }
+    </style>
+</head>
+
+<body>
+	<?php
+
+	$userName = $_POST["userName"];
+	$street   = $_POST["userName"]; 
+	$city     = $_POST["userName"];
+	$state    = $_POST["userName"];
+	$zip      = $_POST["userName"];
+
+	?>
+
+	</body>
+	</html>
+
+
+  
---
+++ userClass.php
@@ -0,0 +1,19 @@
+<?php
+	Class User
+{
+	private userID
+	private userName
+	private firstName
+	private lastName
+	private passWord
+	private admin
+
+	public function isAdmin()
+	{
+		if 
+			return True;
+	}
+
+}
+?>
+
